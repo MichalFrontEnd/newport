f48cd0742a52901cd56a8b32c8749e50fb334ebb
Michal <michal.front@init.de>
2024-02-05 16:00:26 +0100

    Small fixes

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -8,21 +8,20 @@ export const Projects = () => {
         className="projects container container--reverse"
         aria-labelledby="projects"
       >
-        {/* <div className="projects__container projects__container--prof"> */}
-        {/* <h2 className="projects__sub-header">Professional</h2> */}
-        <ProjectsCard type="Professional">
+        <ProjectsCard
+          type="Professional"
+          className="projects__container projects__container--prof"
+        >
           <div className="projects__slider">Slider placeholder</div>
         </ProjectsCard>
-        {/* </div> */}
       </section>
       <section className="projects container container--light">
-        {/* <div className="projects__container projects__container--private"> */}
-        {/* <h2 className="projects__sub-header">Private</h2> */}
-
-        <ProjectsCard type="Private">
+        <ProjectsCard
+          type="Private"
+          className="projects__container projects__container--private"
+        >
           <div className="projects__slider">Slider placeholder</div>
         </ProjectsCard>
-        {/* </div> */}
       </section>
     </>
   );
