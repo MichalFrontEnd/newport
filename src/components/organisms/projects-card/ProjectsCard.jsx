9ca343dd9be0f90343aa968df56854ce5d753139
Michal <michal.front@init.de>
2024-02-05 15:55:04 +0100

    Sets up Project sections

---
+++ src/components/organisms/projects-card/ProjectsCard.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+// import { Image } from "../../molecules";
+import classNames from "classnames";
+// import { Link } from "react-scroll";
+// const src = "/assets/images/mfrontc.png";
+
+export const ProjectsCard = ({ children, type, className }) => {
+  const additionalClassNames = {};
+  if (className !== undefined) {
+    additionalClassNames[className] = true;
+  }
+  return (
+    <div
+      className={classNames("projects-card", additionalClassNames)}
+      aria-labelledby="projects-card"
+    >
+      <h2 className="projects-card__heading">Projects</h2>
+      <h3 className="projects-card__sub-heading">{type}</h3>
+
+      {children && children}
+    </div>
+  );
+};
+
+export default ProjectsCard;
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ProjectsCard } from "../organisms";
 
 export const Projects = () => {
   return (
@@ -7,16 +8,21 @@ export const Projects = () => {
         className="projects container container--reverse"
         aria-labelledby="projects"
       >
-        <div className="projects__container projects__container--prof">
-          <h2 className="projects__sub-header">Professional</h2>
+        {/* <div className="projects__container projects__container--prof"> */}
+        {/* <h2 className="projects__sub-header">Professional</h2> */}
+        <ProjectsCard type="Professional">
           <div className="projects__slider">Slider placeholder</div>
-        </div>
+        </ProjectsCard>
+        {/* </div> */}
       </section>
       <section className="projects container container--light">
-        <div className="projects__container projects__container--private">
-          <h2 className="projects__sub-header">Private</h2>
+        {/* <div className="projects__container projects__container--private"> */}
+        {/* <h2 className="projects__sub-header">Private</h2> */}
+
+        <ProjectsCard type="Private">
           <div className="projects__slider">Slider placeholder</div>
-        </div>
+        </ProjectsCard>
+        {/* </div> */}
       </section>
     </>
   );
